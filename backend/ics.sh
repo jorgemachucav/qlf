ec4fb5fad6610d8d6e265697fa18740b7f66dd89
JMV <47221983+jorgemachucav@users.noreply.github.com>
2019-02-25 00:15:42 -0300

    Update ics.sh

--- backend/ics.sh
+++
@@ -1,9 +0,0 @@
-source /software/products/eups-2.1.4/bin/setups.sh 
-setup DOSlib
-setup QLFInterface
-
-export QLF_ROOT=$(pwd)
-
-export PYTHONPATH=$QLF_ROOT/framework/bin:$PYTHONPATH
-
-python -Wi $QLF_ROOT/framework/bin/ics_daemon.py
